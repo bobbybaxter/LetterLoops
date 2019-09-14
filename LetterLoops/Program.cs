78c158d7acd510de2d31f5591b3d01056dd49cec
Bob <bob.e.baxter@gmail.com>
2019-09-14 18:53:13 -0500

    Add project files.

---
+++ LetterLoops/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LetterLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.Read();
+            
+        }
+    }
+}
